921ff896c42652ac4617fa1318c2f42127720f70
Teemu Kaukoranta <teemu.kaukoranta@gmail.com>
2017-03-01 11:31:32 +0200

    Initial commit

---
+++ project.clj
@@ -0,0 +1,19 @@
+(defproject taistelujaska "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [org.clojure/data.priority-map "0.0.7"]
+                 [clj-http "3.4.1"]
+                 [astar-search "0.2"]]
+
+  :source-paths ["src"]
+  :test-paths ["test"]
+
+  :repl-options
+  {:init-ns taistelujaska.core
+   :timeout 900000 ; 120s, needed for slow machines
+   :port 61815}
+  :main taistelujaska.core)
